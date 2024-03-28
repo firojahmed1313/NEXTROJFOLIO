ebfc68a102b4efe321c5f768e786dae303d0d979
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-03-28 14:29:22 +0000

    pages

--- rojfolio/app/page.js
+++ rojfolio/app/page.js
@@ -1,9 +1,17 @@
+import NavberProfile from "@/components/NavberProfile";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h2>MD FIROJ AHMED</h2>
+      <div className="container border border-red-500 h-[90vh] flex ">
+          <div className="border border-blue-500 w-1/5 m-1">
+            <NavberProfile/>
+          </div>
+          <div className="border border-black w-4/5 m-1">
+            
+          </div>
+      </div>
     </>
   );
 }
---
+++ rojfolio/components/NavberProfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavberProfile = () => {
+  return (
+    <div>NavberProfile</div>
+  )
+}
+
+export default NavberProfile
