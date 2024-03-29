8deb3e656e1f081cb0317cd55eaef1b851aad4b8
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-03-29 05:17:22 +0000

    add layout

--- rojfolio/app/layout.js
+++ rojfolio/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavberProfile from "@/components/NavberProfile";
+import Navber from "@/components/Navber";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container border border-red-500 h-[90vh] flex ">
+          <div className="border border-blue-500 w-1/5 m-1">
+            <NavberProfile />
+          </div>
+          <div className="border border-black w-4/5 m-1">
+            <div className="flex align-middle justify-center border border-green-900 flex-col">
+              <div className="border border-gray-800 h-[10dvh]">
+                <Navber />
+              </div>
+              <div className="border border-gray-800 h-[70vh]">
+                {children}
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </body>
     </html>
   );
 }
---
+++ rojfolio/components/Navber.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Navber = (promts) => {
+    console.log(promts);
+  return (
+    <div>Navber</div>
+  )
+}
+
+export default Navber
